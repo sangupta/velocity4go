ec2a60b2b631c0c16073289e7e991c5682fb5542
Sandeep Gupta <sandy.pec@gmail.com>
2022-04-20 22:14:44 -0700

    refactor to extract utils, updated method docs

---
+++ parser/utils.go
@@ -0,0 +1,130 @@
+package parser
+
+import "sangupta.com/velocity4go/node"
+
+/**
+ * Denotes end of file as a rune
+ */
+const EOF = -1
+
+/**
+ * Check if node is a type of `node.EofNode`
+ */
+func isEofNode(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(*node.EofNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.EndNode`
+ */
+func isEndNode(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(*node.EndNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.ElseIfNode`
+ */
+func isElseIfNode(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(*node.ElseIfNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.ElseIfNode` or `node.EndNode`
+ */
+func isElseOrElseIfEndNode(localNode node.Node) bool {
+	_, ok := localNode.(*node.ElseNode)
+	if ok {
+		return true
+	}
+
+	_, ok = localNode.(*node.ElseIfNode)
+	if ok {
+		return true
+	}
+
+	_, ok = localNode.(*node.EndNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.SetNode`
+ */
+func isSetNodeInstance(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(*node.SetNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.ReferenceNode`
+ */
+func isReferenceNode(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(node.ReferenceNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.CommentNode`
+ */
+func isCommentNode(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(*node.CommentNode)
+	return ok
+}
+
+/**
+ * Check if node is a type of `node.DirectiveNode`
+ */
+func isDirectiveNode(localNode node.Node) bool {
+	if localNode == nil {
+		return false
+	}
+
+	_, ok := localNode.(node.DirectiveNode)
+	return ok
+}
+
+func shouldRemoveLastNodeBeforeSet(nodes []node.Node) bool {
+	length := len(nodes)
+	if length < 2 {
+		return false
+	}
+
+	potentialSpaceBeforeSet := nodes[length-1]
+	beforeSpace := nodes[length-2]
+
+	if isReferenceNode(beforeSpace) {
+		return potentialSpaceBeforeSet.IsHorizontalWhitespace()
+	}
+
+	if isCommentNode(beforeSpace) || isDirectiveNode(beforeSpace) {
+		return potentialSpaceBeforeSet.IsWhitespace()
+	}
+
+	return false
+}
