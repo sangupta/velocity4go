8142a20bef2c23936cd31a522b7bf7c82cc10b75
Sandeep Gupta <sandy.pec@gmail.com>
2022-04-20 22:26:29 -0700

    add copyright header

--- parser/utils.go
+++ parser/utils.go
@@ -1,3 +1,14 @@
+/**
+ * velocity4go: Velocity template engine for Go
+ * https://sangupta.com/projects/velocity4go
+ *
+ * MIT License.
+ * Copyright (c) 2022, Sandeep Gupta.
+ *
+ * Use of this source code is governed by a MIT style license
+ * that can be found in LICENSE file in the code repository.
+ */
+
 package parser
 
 import "sangupta.com/velocity4go/node"
---
+++ parser/utils_test.go
@@ -0,0 +1,21 @@
+/**
+ * velocity4go: Velocity template engine for Go
+ * https://sangupta.com/projects/velocity4go
+ *
+ * MIT License.
+ * Copyright (c) 2022, Sandeep Gupta.
+ *
+ * Use of this source code is governed by a MIT style license
+ * that can be found in LICENSE file in the code repository.
+ */
+
+package parser
+
+import "testing"
+
+func TestIsEofNode(t *testing.T) {
+	if isEofNode(nil) != false {
+		t.Errorf("Invalid EofNode detected")
+	}
+
+}
